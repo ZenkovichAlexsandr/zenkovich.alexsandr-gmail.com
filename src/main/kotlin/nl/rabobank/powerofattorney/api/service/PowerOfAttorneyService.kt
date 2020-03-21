8053813da1c2f7a46b602c0d22971240f4151f7c
a.zenkovich <a.zenkovich@itransition.com>
2020-03-21 18:05:01 +0100

    Remove completable future from `PowerOfAttorneyService`.

--- src/main/kotlin/nl/rabobank/powerofattorney/api/service/PowerOfAttorneyService.kt
+++ src/main/kotlin/nl/rabobank/powerofattorney/api/service/PowerOfAttorneyService.kt
@@ -1,13 +1,15 @@
 package nl.rabobank.powerofattorney.api.service
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import nl.rabobank.powerofattorney.api.model.PowerOfAttorney
 import nl.rabobank.powerofattorney.api.model.PowerOfAttorneyList
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.beans.factory.annotation.Value
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CompletableFuture.allOf
 
 @Service
 class PowerOfAttorneyService {
@@ -16,22 +18,15 @@ class PowerOfAttorneyService {
     @Autowired
     private lateinit var restTemplate: RestTemplate
 
-    fun findByUser(userId: String): List<PowerOfAttorney> {
-        val list = restTemplate
+    suspend fun findByUser(userId: String) = coroutineScope {
+        restTemplate
                 .getForObject(url, Array<PowerOfAttorneyList>::class.java)
-                ?.map { findById(it.id) }
-
-        return list?.let { value ->
-            allOf(*value.toTypedArray())
-                    .thenApply {
-                        value.map { it.join() }
-                    }.join()
-                    .filterNotNull()
-                    .filter { it.grantee == userId }
-        } ?: listOf()
-    }
-
-    private fun findById(id: String) = CompletableFuture.supplyAsync {
-        restTemplate.getForObject("$url/$id", PowerOfAttorney::class.java)
+                ?.map {
+                    async(Dispatchers.IO) {
+                        restTemplate.getForObject("$url/${it.id}", PowerOfAttorney::class.java)
+                    }
+                }?.awaitAll()
+                ?.filterNotNull()
+                ?.filter { it.grantee == userId } ?: listOf()
     }
 }
